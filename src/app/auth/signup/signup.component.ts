a9be5911d1a84d8a74e9a560eb90e7e3d9561a47
anjezzaa <anjeza.murati@gmail.com>
2026-01-29 15:45:22 +0100

    nested form groups

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators, ɵInternalFormsSharedModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -16,11 +16,40 @@ export class SignupComponent {
         Validators.email, 
         Validators.required]
       }),
-      password: new FormControl('', {
-        validators: [
-          Validators.required,
-          Validators.minLength(6)],
+      passwords: new FormGroup({
+        password: new FormControl('', {
+          validators: [
+            Validators.required,
+            Validators.minLength(6)],
+          }),
+          confirmPassword: new FormControl({
+            validators: [
+              Validators.required,
+              Validators.minLength(6),
+            ],
+          }),
+      }),
+
+        
+        firstName: new FormControl('', {
+          validators: [
+            Validators.required,
+          ]
         }),
+        lastName: new FormControl('', {
+          validators: [
+            Validators.required,
+          ]
+        }),
+        address: new FormGroup({
+          street: new FormControl('', {validators: [Validators.required]}),
+          number: new FormControl('', {validators: [Validators.required]}),
+          postalCode: new FormControl('', {validators: [Validators.required]}),
+          city: new FormControl('', {validators: [Validators.required]}),
+        }),
+        role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'
+        >('student', {validators: [Validators.required] }),
+        agree: new FormControl(false, {validators: [Validators.required] }),
       });
       
     onSubmit(){
